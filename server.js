87a005f5b7318e22c316cd54a0352663237a53e7
AntonioAdel <tony.garas95@gmail.com>
2020-02-25 20:33:27 +0200

    port change

--- server.js
+++ server.js
@@ -62,5 +62,7 @@ app.get("/users", (req, resp) => {
   ]);
 });
 
+
+app.listen(process.env.PORT); // create node server
 app.listen(process.env.PORT||3000); // create node server
 //app.listen(3000); // create node server
