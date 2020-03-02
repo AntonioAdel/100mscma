baa5c850dfc8b0581a9af6f09b5b6e90f529c458
100MSCMA <her_email>
2020-03-02 02:08:29 +0200

    sfdsds

--- apis/hospital.js
+++ apis/hospital.js
@@ -87,7 +87,8 @@ function HospitalAPIS(app) {
 
 
   app.get("/getallhospitals", async (req, resp) => {
-    let hospitals = await HospitalModel.find({}).project({ _id : true, /*serialno : 1*/ }).populate("medical_org").project({ _id : true, name:true, address:true, pic_url:true });
+    let hospitals = await HospitalModel.find({}).project({ _id : true, /*serialno : 1*/ }).populate("medical_org");
+    //.project({ _id : true, name:true, address:true, pic_url:true });
     resp.json(hospitals);
   });
 
