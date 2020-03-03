0947ef590bf39cc31d30625e93531b4a40f81d53
100MSCMA <her_email>
2020-03-04 01:33:56 +0200

    oiu

--- apis/medical_organization.js
+++ apis/medical_organization.js
@@ -1,18 +1,16 @@
 //var HospitalModel = require("../models/MedicalOrganization/hospital.model");
 var MedicalOrganizationModel = require("../models/MedicalOrganization/medical.org.model");
-//var PhoneModel = require("../models/common/phone.model");
+//var PhoneModel = require("../models/common/phone.model");//
 
 var mongoose = require("mongoose");
 
 function HospitalAPIS(app) {
-  app.post("/inserthospital", async (req, resp) => {
+  app.get("/gethospitals", async (req, resp) => {
     try {
-      app.get("/gethospitals", async (req, resp) => {
-        let hospitals = await MedicalOrganizationModel.findOne({
-          type: "Hospital"
-        });
-        resp.json(hospitals);
+      let hospitals = await MedicalOrganizationModel.findOne({
+        type: "Hospital"
       });
+      resp.json(hospitals);
     } catch (e) {
       resp.json(e);
     }
